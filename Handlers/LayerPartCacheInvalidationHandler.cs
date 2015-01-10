fbcff075692acb277478bc9377d105efdece11c7
paynecrl97 <chris.payne@bedegaming.com>
2015-01-10 19:03:14 +0000

    added cached layers features

---
+++ Handlers/LayerPartCacheInvalidationHandler.cs
@@ -0,0 +1,21 @@
+﻿using CJP.OutputCachedParts.Filters;
+using Orchard.Caching.Services;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Environment.Extensions;
+using Orchard.Widgets.Models;
+
+namespace CJP.OutputCachedParts.Handlers
+{
+    [OrchardFeature("CJP.OutputCachedParts.CachedWidgetFilter")]
+    public class LayerPartCacheInvalidationHandler : ContentHandler
+    {
+        private readonly ICacheService _cacheService;
+
+        public LayerPartCacheInvalidationHandler(ICacheService cacheService)
+        {
+            _cacheService = cacheService;
+
+            OnUpdated<LayerPart>((ctx, part) => _cacheService.Remove(CachedWidgetFilter.AllLayersCacheKey));
+        }
+    }
+}
---
+++ Models/CachedLayerModel.cs
@@ -0,0 +1,8 @@
+namespace CJP.OutputCachedParts.Models {
+    public class CachedLayerModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Rule { get; set; }
+    }
+}
