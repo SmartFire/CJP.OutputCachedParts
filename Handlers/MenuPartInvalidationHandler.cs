03e4a5e6082857176695c0cf08d12fad63ba9f04
paynecrl97 <chris.payne@bedegaming.com>
2015-05-07 15:31:36 +0100

    removes reference to menu record ready for orchard 1.9

--- Handlers/MenuPartInvalidationHandler.cs
+++ Handlers/MenuPartInvalidationHandler.cs
@@ -78,7 +78,7 @@ namespace CJP.OutputCachedParts.Handlers
                 foreach (var menuId in relevantMenuIds)
                 {
                     var scopedMenuId = menuId;
-                    var menuWidgets = _contentManager.Query<MenuWidgetPart, MenuWidgetPartRecord>().Where(r => r.Menu.Id == scopedMenuId).List();
+                    var menuWidgets = _contentManager.Query<MenuWidgetPart>().List().Where(p => p.MenuContentItemId == scopedMenuId); //todo: can this be made more efficient?
 
                     _outputCachedPartsService.InvalidateCachedOutput(menuWidgets);
                 }
